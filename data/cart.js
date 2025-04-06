d93ed50deb679216e5db2fc813e89b0ba2ff1114
lohithadevi <lohithadevi1835@gmail.com>
2025-04-06 17:51:55 +0530

    Delete products from cart when clicking delete

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,4 @@
-export const cart=[
+export let cart=[
   {
     productId:'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
     quantity:2
@@ -30,5 +30,15 @@ else
     );
     
 }
-console.log(cart);
+}
+
+export function removeFromCart(productId)
+{
+  const newcart=[];
+  cart.forEach((cartItem)=>
+  {
+    if(cartItem.productId!==productId)
+      newcart.push(cartItem);
+  })
+  cart=newcart;
 }
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,4 @@
-import {cart} from '../data/cart.js'
+import {cart,removeFromCart} from '../data/cart.js'
 import { products } from '../data/products.js'
 import {formatCurrency} from './utils/money.js'
 let cartSummaryHTML='';
@@ -35,7 +35,7 @@ cartSummaryHTML+=
                   <span class="update-quantity-link link-primary">
                     Update
                   </span>
-                  <span class="delete-quantity-link link-primary">
+                  <span class="delete-quantity-link link-primary js-delete-link " data-product-id="${matchingProduct.id}">
                     Delete
                   </span>
                 </div>
@@ -88,7 +88,19 @@ cartSummaryHTML+=
             </div>
           </div>`;
 })
-console.log(cartSummaryHTML);
+
 document.querySelector(".js-order-summary").innerHTML=cartSummaryHTML;
+
+
+document.querySelectorAll(".js-delete-link")
+.forEach((link)=>
+{
+  link.addEventListener('click',()=>
+  {
+    const productId=link.dataset.productId;
+    removeFromCart(productId);
+  })
+  
+})
   
 
