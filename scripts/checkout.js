8724e38312d9598264866e8eba5975cb79997149
lohithadevi <lohithadevi1835@gmail.com>
2025-04-06 18:21:26 +0530

    Delete products from HTML when clicking delete

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -12,7 +12,7 @@ let cartSummaryHTML='';
       matchingProduct=product;
   })
 cartSummaryHTML+=
-  `<div class="cart-item-container">
+  `<div class=" cart-item-container js-cart-item-container-${matchingProduct.id}">
             <div class="delivery-date">
               Delivery date: Tuesday, June 21
             </div>
@@ -99,8 +99,11 @@ document.querySelectorAll(".js-delete-link")
   {
     const productId=link.dataset.productId;
     removeFromCart(productId);
-  })
+     const container=document.querySelector(`.js-cart-item-container-${productId}`);
+     container.remove();
+  });
+
   
-})
+});
   
 
