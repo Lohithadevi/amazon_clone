5b3eb46c7bd9c45a41db9414622a5cda231e0ddb
lohithadevi <lohithadevi1835@gmail.com>
2025-04-29 01:58:00 +0530

    add backend practice file

---
+++ data/backendPract.js
@@ -0,0 +1,8 @@
+let xhr=new XMLHttpRequest();
+
+xhr.open('GET',"https://supersimplebackend.dev");
+xhr.send();
+xhr.addEventListener('load',()=>
+  {
+    console.log(xhr.response);
+  });
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -3,7 +3,7 @@ import {renderOrderSummary} from './checkout/orderSummary.js'
 import {renderPaymentSummary} from './checkout/paymentSummary.js'
 
 import {Cart} from'../data/cart-class.js'
-
+import '../data/backendPract.js'
 renderCheckoutHeader();
 renderOrderSummary();
 renderPaymentSummary();
